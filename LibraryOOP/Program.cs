e5a0c066b895dfb7305bdf8c42ca1b7ebbb31818
KateDatema <k8datema@gmail.com>
2021-06-11 11:59:44 -0400

    working on file i/o, not complete yet

--- LibraryOOP/Program.cs
+++ LibraryOOP/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace LibraryOOP
+
+// to update your branch to main 'git rebase main'
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Need to check if this file path works 
+            string filePath = @"/../../../../../Books.txt";
+
+            // pulling in the info from student.txt document
+            StreamReader reader = new StreamReader(filePath);
+            string output = reader.ReadToEnd();
+
+            // splitting each line and putting them in a list
+            string[] lines = output.Split('\n');
+
+            //creating a list to store objects 
+            List<Book> bookList = new List<Book>();
+
+            //adding book objects to a list 
+            foreach (string line in lines)
+            {
+                Book book = ConvertToBookObj(line);
+                if (book != null)
+                {
+                    bookList.Add(book);
+                }
+            }
+
+
+        }
+
+        public static Book ConvertToBookObj(string line)
+        {
+            string[] properties = line.Split(',');
+            Book bookObj = new Book();
+
+            if (properties.Length == 3)
+            {
+                bookObj.Title = properties[0];
+                bookObj.Author = properties[1];
+                bookObj.Status = properties[2];
+                return bookObj;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public static string BookObjToString(Book bookobj)
+        {
+            string output = $"{bookobj.Title}, {bookobj.Author}, {bookobj.Status} \n";
+            return output;
         }
     }
 }
