ba718a688fd67664b9d7ad78ab5155de83085a0b
hainesma <hainesma@gmail.com>
2021-06-14 13:18:01 -0400

    Created DisplayMenu method in Library class

--- LibraryOOP/Library.cs
+++ LibraryOOP/Library.cs
@@ -13,7 +13,50 @@ namespace LibraryOOP
             this.Books = Books;
         }
 
+        public virtual void DisplayMenu(List<Book> bookList)
+        {
+            Console.WriteLine("Library Menu:");
+            Console.WriteLine("1) Display list of books");
+            Console.WriteLine("2) Search by title");
+            Console.WriteLine("3) Search by author");
+            Console.WriteLine("4) Return books");
+            Console.WriteLine("5) Exit the library");
+            Console.WriteLine();
 
+            string input = Console.ReadLine();
+            int choice = int.Parse(input);
+
+            if (choice == 1)
+            {
+                Console.WriteLine("This is what we have in our Library!");
+                Console.WriteLine(" ");
+                foreach (Book bookObj in bookList)
+                {
+                    Console.WriteLine(bookObj.Title);
+                }
+            }
+            else if (choice == 2)
+            {
+                // Search by author
+                List<Book> searchResults = SearchByAuthor();
+                foreach (Book result in searchResults)
+                {
+                    Console.WriteLine(result.Title);
+                }
+            }
+            else if (choice == 3)
+            {
+                // Search by title
+            }
+            else if (choice == 4)
+            {
+                // Return books
+            }
+            else if (choice == 5)
+            {
+                // Exit the library
+            }
+        }
 
         public virtual List<Book> SearchByAuthor()
         {
