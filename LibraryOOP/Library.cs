eadd0cda90bbe32fc59c62f562ee7c377c986a85
hainesma <hainesma@gmail.com>
2021-06-14 13:28:48 -0400

    Completed goOn loop

--- LibraryOOP/Library.cs
+++ LibraryOOP/Library.cs
@@ -15,46 +15,55 @@ namespace LibraryOOP
 
         public virtual void DisplayMenu(List<Book> bookList)
         {
-            Console.WriteLine("Library Menu:");
-            Console.WriteLine("1) Display list of books");
-            Console.WriteLine("2) Search by title");
-            Console.WriteLine("3) Search by author");
-            Console.WriteLine("4) Return books");
-            Console.WriteLine("5) Exit the library");
-            Console.WriteLine();
+            bool goOn = true;
+            while (goOn == true)
+            {
 
-            string input = Console.ReadLine();
-            int choice = int.Parse(input);
 
-            if (choice == 1)
-            {
-                Console.WriteLine("This is what we have in our Library!");
-                Console.WriteLine(" ");
-                foreach (Book bookObj in bookList)
+
+                Console.WriteLine("Library Menu:");
+                Console.WriteLine("1) Display list of books");
+                Console.WriteLine("2) Search by title");
+                Console.WriteLine("3) Search by author");
+                Console.WriteLine("4) Return books");
+                Console.WriteLine("5) Exit the library");
+                Console.WriteLine();
+
+                string input = Console.ReadLine();
+                int choice = int.Parse(input);
+
+                if (choice == 1)
                 {
-                    Console.WriteLine(bookObj.Title);
+                    Console.WriteLine("This is what we have in our Library!");
+                    Console.WriteLine(" ");
+                    foreach (Book bookObj in bookList)
+                    {
+                        Console.WriteLine(bookObj.Title);
+                    }
                 }
-            }
-            else if (choice == 2)
-            {
-                // Search by author
-                List<Book> searchResults = SearchByAuthor();
-                foreach (Book result in searchResults)
+                else if (choice == 2)
                 {
-                    Console.WriteLine(result.Title);
+                    // Search by author
+                    List<Book> searchResults = SearchByAuthor();
+                    foreach (Book result in searchResults)
+                    {
+                        Console.WriteLine(result.Title);
+                    }
                 }
-            }
-            else if (choice == 3)
-            {
-                // Search by title
-            }
-            else if (choice == 4)
-            {
-                // Return books
-            }
-            else if (choice == 5)
-            {
-                // Exit the library
+                else if (choice == 3)
+                {
+                    // Search by title
+                }
+                else if (choice == 4)
+                {
+                    // Return books
+                }
+                else if (choice == 5)
+                {
+                    // Exit the library
+                    Console.WriteLine("Thank you for visiting the Library!");
+                    goOn = false;
+                }
             }
         }
 
